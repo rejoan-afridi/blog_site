3d7bdfa64ec874f555771269ff8bd5c16ee82f96
afridi01 <rejoanafridi93@gmail.com>
2020-10-19 22:31:09 +0600

    mysql database added

---
+++ env.py
@@ -0,0 +1,8 @@
+DB_CONFIG = {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'blog_site',
+    'USER': 'root',
+    'PASSWORD': 'at0991122',
+    'HOST': 'localhost',
+    'PORT': '3306'
+}
