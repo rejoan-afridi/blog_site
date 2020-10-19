32ccae7ab43d5330b7202a898ea1cde21d00ba18
afridi01 <rejoanafridi93@gmail.com>
2020-10-19 23:17:24 +0600

    6

--- os_project/settings.py
+++ os_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = ')t+1by$908dw8*qfo)a9!*vv90_yzg^st04q-aqq98ic%(7l^^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
