6007706815217ac3a0b60985fcebab1c819802bd
afridi01 <rejoanafridi93@gmail.com>
2020-10-19 22:48:57 +0600

    ready to deploy

--- env.py
+++ env.py
@@ -2,7 +2,7 @@ DB_CONFIG = {
     'ENGINE': 'django.db.backends.mysql',
     'NAME': 'blog_site',
     'USER': 'root',
-    'PASSWORD': 'at0991122',
+    'PASSWORD': '',
     'HOST': 'localhost',
     'PORT': '3306'
 }
