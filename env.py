6971c981aedb80a70e3317e4daa82d879a070e18
afridi01 <rejoanafridi93@gmail.com>
2020-10-21 13:23:08 +0600

    10th

--- env.py
+++ env.py
@@ -2,7 +2,7 @@ DB_CONFIG = {
     'ENGINE': 'django.db.backends.mysql',
     'NAME': 'blog_site',
     'USER': 'root',
-    'PASSWORD': '',
+    'PASSWORD': 'at0991122',
     'HOST': 'localhost',
     'PORT': '3306'
 }
